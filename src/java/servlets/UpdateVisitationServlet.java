8b59b13c8005432e6f3915b3ebc2fc3022f9a6cb
Kyle Olive <kolive@uwaterloo.ca>
2014-03-31 13:49:58 -0400

    functionality complete

--- src/java/servlets/UpdateVisitationServlet.java
+++ src/java/servlets/UpdateVisitationServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.User;
-import models.DoctorViewModel;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 /**
@@ -53,44 +53,59 @@ public class UpdateVisitationServlet extends HttpServlet {
             out.println("});");
             out.println("</script>");
                     
-            out.println("<title>New Visitation Record</title>");            
+            out.println("<title>Edit Visitation Record</title>");            
             out.println("</head>");
             out.println("<body class='backgreen'>");
             out.println("<div class='update rounded backteal'>");
-            out.println("<h1>New Visitation Record</h1>");
+            out.println("<h1>Edit Visitation Record</h1>");
             
             User doctor = (User) request.getSession().getAttribute("user");
-            int patientId = Integer.parseInt(request.getParameter("patientId").toString().trim());
             int visitId = Integer.parseInt(request.getParameter("vid").trim());
-            DoctorViewModel dvm = (DoctorViewModel) request.getSession().getAttribute("doctorVM");
+            int patientId = Integer.parseInt(request.getParameter("pid").trim());
             
-            JSONObject visitInfo = Database.getFullVisitRecord(visitId);
+            JSONArray visitInfo = Database.getFullVisitRecord(visitId);
+            System.out.println(visitInfo);
             
             String form = "<form action='UpdateVisitationServlet' method='POST'>";
             
             form += String.format("<input type='hidden' name='eid' value='%s'>", doctor.getStringParam("eid"));
             form += String.format("<input type='hidden' name='vid', value='%s'>", visitId); 
+            form += String.format("<input type='hidden' name='pid', value='%s'>", patientId); 
             
             // Pass as parameter and make it a header
             // need to prepopulate fields
-            form += String.format("<h3>Patient ID: %s </h3> </br>", patientId);
-            form += "<h3>Appointment Date:</h3> <input type='text' class='date' name='visit_date'> </br>";
-            form += "<h3>Start Time:</h3> <input type='text' name='visit_start_time'></br>";
-            form += "<h3>End Time:</h3> <input type='text' name='visit_end_time'></br>";
+            form += String.format("<h3>Visit ID: %s </h3> </br>", visitId);
+            form += String.format("<h3>Appointment Date:</h3> <input type='text' class='date' name='visit_date' value='%s'> </br>", 
+                    ((JSONObject)visitInfo.get(0)).get("visit_date"));
+            form += String.format("<h3>Start Time:</h3> <input type='text' name='visit_start_time' value='%s'></br>",
+                    ((JSONObject)visitInfo.get(0)).get("visit_start_time"));
+            form += String.format("<h3>End Time:</h3> <input type='text' name='visit_end_time' value='%s'></br>",
+                    ((JSONObject)visitInfo.get(0)).get("visit_end_time"));
             
             form += "</br>";
             
-            form += "<h3>Procedure Performed:</h3> <p>Name: <input type='text' name='procedure_name'> Description: <input type='text' name='description'></p></br>";
-            form += "<h3>Diagnosis:</h3> <input type='text' name='severity'></br>";
+            form += String.format("<h3>Procedure Performed:</h3> <p>Name: <input type='text' name='procedure_name' value='%s'>",
+                    ((JSONObject)visitInfo.get(0)).get("procedure_name"));
+            form += String.format("Description: <input type='text' name='description' value='%s'></p></br>",
+                    ((JSONObject)visitInfo.get(0)).get("description"));
+            form += String.format("<h3>Diagnosis:</h3> <input type='text' name='severity' value='%s'></br>",
+                    ((JSONObject)visitInfo.get(0)).get("severity"));
             form += "<h3>Prescriptions Prescribed:</h3> <div>";
-            form += "<p>Drug Name: <input type='text' name='drug_name1'> Expires: <input type='text' class='date' name='expires1'></p>";
-            form += "<p>Drug Name: <input type='text' name='drug_name2'> Expires: <input type='text' class='date' name='expires2'></p>";
-            form += "<p>Drug Name: <input type='text' name='drug_name3'> Expires: <input type='text' class='date' name='expires3'></p>";
-            form += "<p>Drug Name: <input type='text' name='drug_name4'> Expires: <input type='text' class='date' name='expires4'></p>";
-            form += "<p>Drug Name: <input type='text' name='drug_name5'> Expires: <input type='text' class='date' name='expires5'></p>";
+            for(int i = 0; i < visitInfo.size() && ((JSONObject)visitInfo.get(i)).get("drug_name") != null ; i++){
+                form += String.format("<p>Drug Name: <input type='text' name='drug_name%s' value='%s'> Expires: <input type='text' class='date' name='expires%s' value='%s'></p>",
+                        Integer.toString(i), ((JSONObject)visitInfo.get(i)).get("drug_name"),
+                        Integer.toString(i), ((JSONObject)visitInfo.get(0)).get("expires"));
             
-            form += "</div></br>";
-            form += "<h3>Comments:</h3> <input type='text' name='content'></br>";
+            }
+            
+            form += "</div></br>"; 
+            String comment = "";
+            if(((JSONObject)visitInfo.get(0)).containsKey("content") 
+                    && ((JSONObject)visitInfo.get(0)).get("content") != null){
+                comment = ((JSONObject)visitInfo.get(0)).get("content").toString();
+            }
+            form += String.format("<h3>Comments:</h3> <input type='text' name='content'value='%s'></br>",
+                        comment);
             
             form += "<input type='submit' value='Submit'>";
             form += "</form>";
@@ -100,6 +115,10 @@ public class UpdateVisitationServlet extends HttpServlet {
             out.println("</div>");
             out.println("</body>");
             out.println("</html>");
+        }catch(Exception e){
+            e.printStackTrace();            
+            response.sendRedirect("/ece356/error.jsp");
+            out.close();
         }finally {
             out.close();
         }
@@ -131,7 +150,54 @@ public class UpdateVisitationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        JSONObject visitParams = new JSONObject();
+            
+            
+        if(request.getParameter("vid") != null && !request.getParameter("vid").trim().equals("") &&
+            request.getParameter("visit_date") != null && !request.getParameter("visit_date").trim().equals("") &&
+            request.getParameter("visit_start_time") != null && !request.getParameter("visit_start_time").trim().equals("") &&
+            request.getParameter("visit_end_time") != null && !request.getParameter("visit_end_time").trim().equals("")){
+
+            visitParams.put("vid", request.getParameter("vid"));
+            visitParams.put("visit_date", request.getParameter("visit_date"));
+            visitParams.put("visit_start_time", request.getParameter("visit_start_time"));
+            visitParams.put("visit_end_time", request.getParameter("visit_end_time"));
+
+            visitParams.put("procedure_name", request.getParameter("procedure_name"));
+            visitParams.put("description", request.getParameter("description"));
+            visitParams.put("severity", request.getParameter("severity"));
+            visitParams.put("content", request.getParameter("content"));
+            
+            visitParams.put("pid", request.getParameter("pid"));
+
+
+            JSONArray prescriptions = new JSONArray();
+
+            for(int i = 1; i <= 5; i++){
+                JSONObject prescription = new JSONObject();
+
+                if(request.getParameter("drug_name" + i) != null && !request.getParameter("drug_name" + i).trim().equals("") &&
+                        request.getParameter("expires" + i) != null && !request.getParameter("expires" + i).trim().equals("")){
+                    prescription.put("drug_name", request.getParameter("drug_name" + i).trim());
+                    prescription.put("expires", request.getParameter("expires" + i).trim());
+
+                    prescriptions.add(prescription);
+                }   
+            }
+
+            visitParams.put("prescriptions", prescriptions);
+            
+            System.out.println(visitParams);
+            
+            boolean success = Database.UpdateRecord(Integer.parseInt(request.getParameter("eid")), visitParams);
+
+            if(success){
+                System.out.println("<p class='status'>Update visitation record submission successful!</p>");
+            }
+            else{
+                System.out.println("<p class='status'>Update visitation record submission failed. Please try again, or contact your system admin.</p>");
+            }
+        }
     }
 
     /**
